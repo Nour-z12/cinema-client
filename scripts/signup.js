8860c9f9e3c30aa3aa57bf581c96304778dafce2
Nour-z12 <nourbzeidan@gmail.com>
2025-06-25 17:11:55 +0300

    finished the login and fixed the css

--- scripts/signup.js
+++ scripts/signup.js
@@ -4,10 +4,10 @@
 document.getElementById('myForm').addEventListener('submit', async function (e) {
     e.preventDefault();
 //document.getElementById('submit-btn').addEventListener()
-	const username = document.getElementById('username');
-	const email = document.getElementById('email');
-	const mobile = document.getElementById('mobile');
-	const password_hash = document.getElementById('password_hash');
+	const username = document.getElementById('username').value;
+	const email = document.getElementById('email').value;
+	const mobile = document.getElementById('mobile').value;
+	const password_hash = document.getElementById('password_hash').value;
       try{
         const response = await
         axios.post('http//localhost/cinema-server/controllers/authenticator.php',{
