17cc5facf801541ca9fa8e85ec395ac9e33786e9
Nour-z12 <nourbzeidan@gmail.com>
2025-06-26 13:02:57 +0300

    finished linking with backend

--- scripts/signup.js
+++ scripts/signup.js
@@ -3,23 +3,19 @@
 
 document.getElementById('myForm').addEventListener('submit', async function (e) {
     e.preventDefault();
-//document.getElementById('submit-btn').addEventListener()
-	const username = document.getElementById('username').value;
-	const email = document.getElementById('email').value;
-	const mobile = document.getElementById('mobile').value;
-	const password_hash = document.getElementById('password_hash').value;
-      try{
-        const response = await
-        axios.post('http//localhost/cinema-server/controllers/authenticator.php',{
-            username: username,
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const mobile = document.getElementById('mobile').value;
+    const password_hash = document.getElementById('password_hash').value;
+    try {
+        const response = await axios.post('http://localhost/cinema-server/controllers/authenticator.php', {
+            name: username,
             email: email,
             mobile: mobile,
             password_hash: password_hash
         });
         console.log(response.entry);
-      } catch (error) {
+    } catch (error) {
         console.error(error);
     }
-
-	
 });
