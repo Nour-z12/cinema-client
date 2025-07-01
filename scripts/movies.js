833048e9d0ed99d0a9ab12bd65caf29eb83f2a1d
Nour-z12 <nourbzeidan@gmail.com>
2025-07-02 00:34:04 +0300

    creating pages

--- scripts/movies.js
+++ scripts/movies.js
@@ -2,16 +2,18 @@ document.querySelector('.nav-link-menu').addEventListener('click', function() {
   document.querySelector('.nav-links').classList.toggle('active');
 });
 
+
+
+
 window.addEventListener('DOMContentLoaded', async () => {
     try {
         const response = await axios.get('http://localhost/cenema-server/controllers/get_films.php');
         const movies = response.data.Films;
-        const movieInfo = document.querySelector('#movie-info');
+        const movieSection = document.querySelector('.movie-section');
         for(let movie of movies) {
             movieSection.innerHTML += `
-                <div class="movie-trailor">
-                
-                    
+                <div class="movie-card" onclick= booking(${movie.id})>
+                    <img  src="${movie.image_url}  alt="${movie.title}">
                     <h2>${movie.title}</h2>
                    <div class="movie-info">
                     
@@ -31,4 +33,6 @@ window.addEventListener('DOMContentLoaded', async () => {
         console.error('Error fetching movies:', error);
     }
 });
-
+function booking(id) {
+    window.location.href = `movie${id}.html?id=${id}`;
+}
